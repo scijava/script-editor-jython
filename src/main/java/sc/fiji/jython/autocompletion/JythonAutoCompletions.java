ffb3505866f45e9543092a9468aa47f0fc2f8603
Albert Cardona <sapristi@gmail.com>
2020-12-08 17:04:03 +0000

    Ensure codeWithoutLastLine is valid python code: add "pass" if the code ends with a ':' to complete the statement.
    Of course, adding "pass" is complicated: must avoid comments
    or have the right indentation.

--- src/main/java/sc/fiji/jython/autocompletion/JythonAutoCompletions.java
+++ src/main/java/sc/fiji/jython/autocompletion/JythonAutoCompletions.java
@@ -21,27 +21,42 @@ public class JythonAutoCompletions implements AutoCompletionListener
 	
 	static private final Pattern assign = Pattern.compile("^([ \\t]*)(([a-zA-Z_][a-zA-Z0-9_ \\t,]*)[ \\t]+=[ \\t]+(.*))$"),
 						         nameToken = Pattern.compile("^(.*?[ \\t]+|)([a-zA-Z_][a-zA-Z0-9_]+)$"),
-						         dotNameToken = Pattern.compile("^(.*?[ \\t]+|)([a-zA-Z0-9_\\.\\[\\](){}]+)\\.([a-zA-Z0-9_]*)$");
+						         dotNameToken = Pattern.compile("^(.*?[ \\t]+|)([a-zA-Z0-9_\\.\\[\\](){}]+)\\.([a-zA-Z0-9_]*)$"),
+						     	 endingCode = Pattern.compile("^([ \\t]*)[^#]*?(.*?)[ \\t]*:[ \\t]*(#.*|)[\\n]*$");
 	
 	public JythonAutoCompletions() {}
 	
 	@Override
-	public List<Completion> completionsFor(final CompletionProvider provider, final String codeWithoutLastLine, final String lastLine, final String alreadyEnteredText) {
+	public List<Completion> completionsFor(final CompletionProvider provider, String codeWithoutLastLine, final String lastLine, final String alreadyEnteredText) {
 		
 		// Replacing of text will start at crop, given the already entered text that is considered for replacement
 		final int crop = lastLine.length() - alreadyEnteredText.length();
 		
 		// Query the lastLine to find out what needs autocompletion
 		
-		// Preconditions: can't expand when ending with any of: "()[]{},; "
+		// Preconditions 1: can't expand when ending with any of: "()[]{},; "
 		final char lastChar = lastLine.charAt(lastLine.length() -1);
 		if (0 == lastLine.length() || "()[]{},; ".indexOf(lastChar) > -1)
 			return Collections.emptyList();
 		
+		// Preconditions 2: codeWithoutLastLine has to be valid
+		// Analyze last line of codeWithoutLastLine: if it ends with a ':', must add a "pass" to make it a valid code block
+		// so that the ParserFacade can work
+		if (codeWithoutLastLine.endsWith("\n")) {
+			JythonScriptParser.print("codeWithoutLastLine ends with line break");
+			final int priorLineBreak = codeWithoutLastLine.lastIndexOf('\n', codeWithoutLastLine.length() - 2);
+			String endingLine = codeWithoutLastLine.substring(priorLineBreak);
+			final Matcher me = endingCode.matcher(endingLine);
+			if (me.find()) {
+				codeWithoutLastLine = codeWithoutLastLine.substring(0, priorLineBreak + 1) + me.group(1) + me.group(2) + ": pass";
+				JythonScriptParser.print("changed code to: \n" + codeWithoutLastLine + "\n###");
+			}
+		}
+		
 		// Two situations to autocomplete:
 		// 1) a plain name: delimited with space (or none) to the left, and without parentheses.
 		// 2) a method or field: none or some text after a period.
-		
+
 		final Matcher m1 = nameToken.matcher(lastLine);
 		if (m1.find())
 			return JythonScriptParser.parseAST(codeWithoutLastLine).getLast().findStartsWith(m1.group(2)).stream()
