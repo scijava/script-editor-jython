scijava/scripting-jython

Add SyntaxHighlighter for language name python-(jython)

src/main/java/org/scijava/plugins/scripting/jython/JythonScriptSyntaxHighlighter.java
@@ -1,0 +1,46 @@
+/*
+ * #%L
+ * Python scripting language plugin to be used with PyImageJ.
+ * %%
+ * Copyright (C) 2021 - 2022 SciJava developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.plugins.scripting.jython;
+
+import org.fife.ui.rsyntaxtextarea.modes.PythonTokenMaker;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.swing.script.SyntaxHighlighter;
+
+/**
+ * SyntaxHighlighter for Python (Jython).
+ *
+ * @author Karl Duderstadt
+ */
+@Plugin(type = SyntaxHighlighter.class, name = "python-(jython)")
+public class JythonScriptSyntaxHighlighter extends PythonTokenMaker implements
+SyntaxHighlighter
+{
+	// Everything implemented in PythonTokenMaker
+}

scijava/script-editor-jython

src/main/java/org/scijava/plugins/scripteditor/jython/JythonScriptSyntaxHighlighter.java
@@ -1,0 +1,46 @@
+/*
+ * #%L
+ * Jython language support for SciJava Script Editor.
+ * %%
+ * Copyright (C) 2020 - 2024 SciJava developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.plugins.scripteditor.jython;
+
+import org.fife.ui.rsyntaxtextarea.modes.PythonTokenMaker;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.swing.script.SyntaxHighlighter;
+
+/**
+ * SyntaxHighlighter for Python (Jython).
+ *
+ * @author Karl Duderstadt
+ */
+@Plugin(type = SyntaxHighlighter.class, name = "python-(jython)")
+public class JythonScriptSyntaxHighlighter extends PythonTokenMaker implements
+SyntaxHighlighter
+{
+	// Everything implemented in PythonTokenMaker
+}